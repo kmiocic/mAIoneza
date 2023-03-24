dec848e08115dde62d7935951d5cd77a228f23d2
Majda Bakmaz <majdabakmaz@gmail.com>
2023-03-24 22:34:19 +0100

    stogod

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-# ovo je main python file koji niko nezna još koristiti
+import requests
 
+# Postavljanje URL-a za lokalni poslužitelj
+url = 'http://localhost:8081/user/login'
+
+# Postavljanje podataka koje želite poslati na poslužitelj u obliku rječnika
+data = {'username': 'admin', 'passwork': 'admin'}
+
+# Slanje POST zahtjeva
+response = requests.post(url, data=data)
+
+# Ispisivanje odgovora poslužitelja
+print(response.text)
